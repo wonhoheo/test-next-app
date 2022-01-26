bb9988c66dbf87f1230e949b8e19101c9dd6bbe6
wonho.heo <wonho.heo@greenlabs.co.kr>
2022-01-26 11:03:01 +0900

    불필요 파일 제거

--- lib/bs/src/App.d
+++
@@ -1,1 +0,0 @@
-src/App.cmj : src/layouts/MainLayout.cmj src/layouts/MainLayout.cmi src/bindings/Next.cmj src/bindings/Next.cmi
--- lib/bs/src/layouts/MainLayout.d
+++
@@ -1,1 +0,0 @@
-src/layouts/MainLayout.cmj : src/bindings/Next.cmj src/bindings/Next.cmi
